4f7e63aeb9998f9a25ed7cd5c045721f390137ac
Peter Lüer <lueerp@gmx.de>
2022-03-15 20:19:33 +0100

    Initial Commit

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+import waitress
+import flask
+
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def main():
+    token = flask.request.args.get('token')
+    zone = flask.request.args.get('zone')
+    record = flask.request.args.get('record')
+    ipv4 = flask.request.args.get('ipv4')
+    ipv6 = flask.request.args.get('ipv6')
+    cf = CloudFlare.CloudFlare(token=token)
+
+    if not token:
+        return flask.jsonify({'status': 'error', 'message': 'Missing token URL parameter.'}), 400
+    if not zone:
+        return flask.jsonify({'status': 'error', 'message': 'Missing zone URL parameter.'}), 400
+    if not record:
+        return flask.jsonify({'status': 'error', 'message': 'Missing record URL parameter.'}), 400
+    if not ipv4 and not ipv6:
+        return flask.jsonify({'status': 'error', 'message': 'Missing ipv4 or ipv6 URL parameter.'}), 400
+
+    try:
+
+    return flask.jsonify({'status': 'success', 'message': 'Update successful.'}), 200
+
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(24)
+    waitress.serve(app, host='0.0.0.0', port=80)
+
